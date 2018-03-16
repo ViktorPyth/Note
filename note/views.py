853afb0979c1cf808f43e5abe6b4a76a74a764b4
Viktor <vitya052@inbox.ru>
2018-03-16 23:35:21 +0300

    add new note

--- note/views.py
+++ note/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 
+
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'note/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'note/post_detail.html', {'post': post})
 
+
 def post_new(request):
-    form = PostForm()
-    return render(request, 'post/post_new.html', {'form': form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.like = False
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'note/post_new.html', {'form': form})
